6126899deba85f338918041975fb14794233b762
karim <karimessouabni0@gmail.com>
2019-03-10 19:22:53 +0100

    Initial commit

---
+++ src/main/java/com/kaghimou/functionalapi/FunctionalapiApplication.java
@@ -0,0 +1,13 @@
+package com.kaghimou.functionalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionalapiApplication.class, args);
+    }
+
+}
